10625cebc72b079b49f81c9cd20e9d4a1094c6f8
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 04:20:26 -0400

    sets up Reader validations

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,4 +1,11 @@
 class Reader < ApplicationRecord
     has_many :goals
     has_many :books, through: :goals
+    
+    has_secure_password
+    validates_uniqueness_of :username
+    validates_presence_of :username
+    validates_uniqueness_of :email
+    validates_presence_of :email
+      
 end
