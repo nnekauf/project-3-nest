d0970c461231d9b02d68eb0f538402f0b05ff338
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 04:54:22 -0400

    migrates table to database, seeds data, enables bcrypt and pry

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -3,6 +3,6 @@ class Goal < ApplicationRecord
     has_many :plots #a goal can be acieved through different scenarios/plots
     has_many :books, through: :plots #a goal is associated with different books through the plots that the book has
 
-    validates :description length: { minimum: 10 }
+    validates :description, length: { minimum: 10 }
     validates_presence_of :completion_date
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Reader.create([{ username: 'DeniseReads', email: 'denisereadsexample@gmail.com', password: 'password' }, {username: 'sallyreads', email: 'sallyreadsexample@gmail.com', password: 'password'  }, { username: 'CharlieReads', email: 'charliereadsexample@gmail.com', password: 'password'  }])
+
+Goal.create([{reader_id: 1, description: "I want to learn more about gardening", completion_date: 2021-9-25}, {reader_id: 2, description: "I want to learn start my own business", completion_date: 2021-6-25}, {reader_id: 3, description: "I want to be more romantic towards my spouse", completion_date: 2021-10-25}])
+
+
+Plot.create([{description: "Get all of your gardeninghacks here!", goal_id: 1, book_id: 1}, {description: "Where business minded people learn the keys to success", goal_id: 2, book_id: 2}, {description: "Modern day romeo and juliet", goal_id: 3, book_id: 3}])
+
+Book.create([{title: "Gardening 101", author: "Misses Greenthumb"}, {title: "Self-Made 101", author: "Johhny Joe"}, {title: "Romance 101", author: "Dr. Love"}])
