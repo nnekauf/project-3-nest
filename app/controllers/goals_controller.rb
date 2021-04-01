29757239a61d1184f9ca1ebc0d2719f14e8fceed
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 14:36:11 -0400

    establishes Goal routes and corresponding controller actions

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,42 @@
 class GoalsController < ApplicationController
+    def index
+        @goals = Goal.all
+      end
+    
+      def show
+        @goal = Goal.find_by(id: params[:id])
+      end
+    
+      def new
+        @goal = Goal.new
+      end
+    
+      def create
+        item = Goal.create(goal_params)
+        redirect_to goal_path(goal)
+      end
+    
+      def edit
+        @goal = Goal.find_by(id: params[:id])
+      end
+    
+      def update
+        item = Goal.find_by(id: params[:id])
+        item.update(goal_params)
+        redirect_to(goal_path(goal))
+      end
+    
+    
+      def destroy
+        item = Goal.find_by(id: params[:id])
+        item.delete
+        redirect_to goals_path
+      end
+    
+    
+      private
+    
+        def goal_params
+          params.require(:goal).permit(:description, :completion_date)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :plots
+  # resources :books
+  # resources :plots
   resources :goals
-  resources :readers
+  # resources :readers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
