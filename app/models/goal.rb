f39f3edbf292783809956779f935350b2aa3f516
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 04:24:14 -0400

    sets up Goal validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,7 @@ class Goal < ApplicationRecord
     belongs_to :reader #a goal belongs to the person who sets it
     has_many :plots #a goal can be acieved through different scenarios/plots
     has_many :books, through: :plots #a goal is associated with different books through the plots that the book has
+
+    validates :description length: { minimum: 10 }
+    validates_presence_of :completion_date
 end
