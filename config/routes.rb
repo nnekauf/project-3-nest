1c6bbe6490143962fd18672222990e4a9ff94ae8
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 20:12:44 -0400

    sets up sign up and login routes along with the corresponding view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :readers do #readers have goals
+  resources :readers, except: [:new] do #readers have goals
     resources :goals
   end
 
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :goals
   # resources :books
   # resources :plots
-  
-  
+  get '/signup', to: "readers#new", as: "new_reader"
+  get '/login', to: "sessions#new", as: "new_reader"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
