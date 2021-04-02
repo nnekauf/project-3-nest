12a0e7a7e82e4fbeaa07cc6151a36ed743f7fe3a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-01 23:40:31 -0400

    sets up facebook routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#create_with_fb"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
