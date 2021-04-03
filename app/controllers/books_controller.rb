e43e5ce879f66c7bc9b5e38c4c903d6469feb21c
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-02 23:03:58 -0400

    sets up bare bones book controller actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,21 @@ class BooksController < ApplicationController
     def index 
 
     end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def destroy
+        
+    end
+
 end
