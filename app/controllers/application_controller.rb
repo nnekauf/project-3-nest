3857ba9521a2f5133c756bd5e905ea4bdab6a72d
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-03 01:31:42 -0400

    adds show helper and get class name methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,14 @@ class ApplicationController < ActionController::Base
         class_name = self.get_class_name
         instance_variable_set("@#{params["controller"]}", class_name.all)
     end
+
+    def show_helper
+        class_name = self.get_class_name
+        instance_variable_set("@#{params["controller"].singularize}", class_name.find_by(id: params[:id]))
+    end
+    
+    def get_class_name
+        Object.const_get(params["controller"].titlecase.singularize)
+    end
+
 end
