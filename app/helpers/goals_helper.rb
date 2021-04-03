4c8e7ce391269f6a57f98cbde9ca0a7eb534af11
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-03 02:29:32 -0400

    adds Goals helper methods

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,2 +1,13 @@
 module GoalsHelper
-end
+
+    def show_book_title(g, index)
+        g.book.title if index
+    end
+  
+  
+    def form_url_helper(book)
+      book ? book_goals_path(book) : goals_path
+    end
+  
+  end
+  
