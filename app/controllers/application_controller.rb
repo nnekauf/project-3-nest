b7e0f3f46f8895d5e2abf9c4c02f6fbfac8d2a06
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-03 01:28:11 -0400

    adds require logon and index helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
     def current_user
         @current_user = Reader.find_by(id: session[:reader_id])
     end
+
+    def require_login
+        ((flash[:message] = "You must be logged in to do that") && (redirect_to '/login')) if !current_user
+    end
+    
+    def index_helper
+        class_name = self.get_class_name
+        instance_variable_set("@#{params["controller"]}", class_name.all)
+    end
 end
