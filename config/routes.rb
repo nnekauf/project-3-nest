6e39281b1238e33bd351bb822336b448d6c507d6
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-03 02:20:53 -0400

    reverts route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
-Rails.application.routes.draw do
-  resources :readers, except: [:new, :create] do #readers have goals
-    resources :(goals, except: [:show, :edit, :update, :destroy, :put])
-    resources :(books, except: [:show, :edit, :update, :destroy, :put])
-  end
 
-  resources (books, :except: [:put, :edit, :patch, :destroy, :update]) do
-    resources :goals, only: [:new, :create]
-  end
+  Rails.application.routes.draw do
+    resources :readers, except: [:new, :create] do #readers have goals
+      resources :goals
+      resources :books
+    end
   
-
-  resources :goals, only: [:new, :create, :index, :edit, :update]
-  resources :books
-  # resources :plots
-  get '/signup', to: "readers#new", as: "signup"
-  post '/signup', to: "readers#create"
-  get '/login', to: "sessions#new", as: "login"
-  post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#destroy"
-  get "/auth/facebook/callback", to: "sessions#create_with_fb"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+    resources :goals do #goals have many plots associated with them
+      resources :plots
+    end
+  
+    resources :goals
+    resources :books
+    # resources :plots
+    get '/signup', to: "readers#new", as: "signup"
+    post '/signup', to: "readers#create"
+    get '/login', to: "sessions#new", as: "login"
+    post '/login', to: "sessions#create"
+    post '/logout', to: "sessions#destroy"
+    get "/auth/facebook/callback", to: "sessions#create_with_fb"
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
