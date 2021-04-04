cf3799ae8ee9348dbd3df38844d1768ecb40b416
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-04 19:02:01 -0400

    customizes index page to show username

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,8 @@ layout 'application'
         @books = Book.title_search(params[:title].to_s)
         # @books = Book.title_search(params[:title].to_s)
       elsif params[:reader_id]
-        reader = Reader.find_by(id: params[:reader_id])
-        @books = reader.books
+        @reader = Reader.find_by(id: params[:reader_id])
+        @books = @reader.books
       elsif params[:author_id]
         author = Author.find_by(id: params[:author_id])
         @books = author.books
