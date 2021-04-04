c5069c29fd15960ca13daf563a8fa6cb63deae75
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-04 05:30:45 -0400

    fixes the layout

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
     before_action(:require_login)
     # before_action(:show_helper, only: [:edit, :update])
     # before_action(:index_helper, only: :index)
-
+layout 'application'
     def index
       
       if params[:title]
