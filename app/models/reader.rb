a860a5cd1335cf6dd525f3d74c9e3f9c64d9cf2f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 19:52:34 -0400

    sets up has many relationships, secure password, and validations

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,2 +1,13 @@
 class Reader < ApplicationRecord
+    has_many :reviews
+    has_many :books, through: reviews
+
+    has_secure_password
+
+    validates_uniqueness_of :username
+    validates_presence_of :username
+    
+    
+    validates_uniqueness_of :email
+    validates_presence_of :email
 end
