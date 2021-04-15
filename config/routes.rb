7debee5e0f315489fa690e881d512eb524c43a08
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 19:46:54 -0400

    comments out all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,28 @@
 
   Rails.application.routes.draw do
 
-  resources :reviews
-    resources :readers, except: [:new, :create] do #readers have books
-      resources :books
-    end
+  # resources :reviews
+  #   resources :readers, except: [:new, :create] do #readers have books
+  #     resources :books
+  #   end
   
-    resources :authors do 
-      resources :books
-    end
+  #   resources :authors do 
+  #     resources :books
+  #   end
   
    
     
-    resources :books do
-      resources :readers
-      resources :authors
-    end
-    # resources :plots
-    get '/home', to: "readers#home"
-    get '/signup', to: "readers#new", as: "signup"
-    post '/signup', to: "readers#create"
-    get '/login', to: "sessions#new", as: "login"
-    post '/login', to: "sessions#create"
-    post '/logout', to: "sessions#destroy"
-    get "/auth/facebook/callback", to: "sessions#create_with_fb"
+  #   resources :books do
+  #     resources :readers
+  #     resources :authors
+  #   end
+  #   # resources :plots
+  #   get '/home', to: "readers#home"
+  #   get '/signup', to: "readers#new", as: "signup"
+  #   post '/signup', to: "readers#create"
+  #   get '/login', to: "sessions#new", as: "login"
+  #   post '/login', to: "sessions#create"
+  #   post '/logout', to: "sessions#destroy"
+  #   get "/auth/facebook/callback", to: "sessions#create_with_fb"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
