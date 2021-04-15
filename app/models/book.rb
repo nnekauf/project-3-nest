7c59f7ae2d8a55ebfd64cceeb4b632f93c995377
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 19:48:46 -0400

    sets up scope and has many relationships

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+    has_many :reviews
+    has_many :readers, through: reviews
+
+    scope(:title_search, ->(title) { self.where("title like ?", "%#{title}%") })
 end
