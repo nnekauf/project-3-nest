f63a3fd0fa591009668c2c42b8a962db940771b5
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 22:39:17 -0400

    adds sign up with facebook route and button to sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,6 @@
     # get '/login', to: "sessions#new", as: "login"
     # post '/login', to: "sessions#create"
   #   post '/logout', to: "sessions#destroy"
-  #   get "/auth/facebook/callback", to: "sessions#create_with_fb"
+    get "/auth/facebook/callback", to: "sessions#create_with_fb"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
