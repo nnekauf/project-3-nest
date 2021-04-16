3408347337771110feab950a73d4e2de8ff286c2
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-16 09:19:53 -0400

    moves helper methods to review

--- app/helpers/readers_helper.rb
+++ app/helpers/readers_helper.rb
@@ -1,13 +1,4 @@
 module ReadersHelper
 
-    def check_owner(obj)
-        obj.reader == current_user
-      end
-  
-      def redirect_if_not_owner(obj)
-        if !check_owner(obj)
-          flash[:message] = "Sorry, this is not your review!"
-          redirect "/home"
-        end
-      end
+    
 end
