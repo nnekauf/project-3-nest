ea075f18bb6e41cbe0d5ea0fb4ba7ef88dfc18ed
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 23:31:30 -0400

    index accounts for search params

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,20 @@
 class BooksController < ApplicationController
 
     layout 'application'
-    
+
     def index
       
-        # if params[:title]
-        #   @books = Book.title_search(params[:title].to_s)
-        #   # @books = Book.title_search(params[:title].to_s)
+        if params[:title]
+          @books = Book.title_search(params[:title].to_s)
         # elsif params[:reader_id]
         #   @reader = Reader.find_by(id: params[:reader_id])
         #   @books = @reader.books
         # elsif params[:author_id]
         #   author = Author.find_by(id: params[:author_id])
         #   @books = author.books
-        # else 
+        else 
           @books = Book.all
-        # end
+        end
       
       end
 
