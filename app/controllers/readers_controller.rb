3a1bff1a3f3dfa83300e3498c2e2892c1ca5c4ef
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 22:31:54 -0400

    adds private reager_params strong params

--- app/controllers/readers_controller.rb
+++ app/controllers/readers_controller.rb
@@ -8,6 +8,21 @@ class ReadersController < ApplicationController
     end
 
     def create
-
+        @reader = Reader.new(reader_params)
+        if @reader.save
+            session[:reader_id] = @reader.id
+            flash[:message] = "Successfully created account!"
+            redirect_to "/home"
+        else
+            @errors = @reader.errors.full_messages
+            render :new
+        end
     end
+    
+    private
+
+    def reader_params
+      params.require(:reader).permit(:username, :email, :password, :password_confirmation)
+    end
+  
 end
