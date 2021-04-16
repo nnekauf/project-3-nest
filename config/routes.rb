e074718ffb2b2c8f124add90ab0c496a16d8534f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 23:10:39 -0400

    adds readers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 
   Rails.application.routes.draw do
+    get '/', to: "readers#welcome"
 
+    get '/signup', to: "readers#new", as: "signup"
+    post '/signup', to: "readers#create"
+
+    get '/login', to: "sessions#new", as: "login"
+    post '/login', to: "sessions#create"
+    post '/logout', to: "sessions#destroy"
+    
+    get "/auth/facebook/callback", to: "sessions#create_with_fb"
+
+    resources :readers
   # resources :reviews
   #   resources :readers, except: [:new, :create] do #readers have books
   #     resources :books
@@ -18,12 +29,6 @@
   #   end
   #   # resources :plots
     # get '/home', to: "readers#home"
-    get '/', to: "readers#welcome"
-    get '/signup', to: "readers#new", as: "signup"
-    post '/signup', to: "readers#create"
-    get '/login', to: "sessions#new", as: "login"
-    post '/login', to: "sessions#create"
-    post '/logout', to: "sessions#destroy"
-    get "/auth/facebook/callback", to: "sessions#create_with_fb"
+  
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
