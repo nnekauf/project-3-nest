1a7aeebadbcce2d1094db12547e8c7bffaa99928
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-16 03:51:51 -0400

    fixes formatting of home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,16 @@
   Rails.application.routes.draw do
     get '/', to: "readers#welcome" #all good
 
-    get '/signup', to: "readers#new", as: "signup"
+    get '/signup', to: "readers#new", as: "signup" #all good
     post '/signup', to: "readers#create"
 
-    get '/login', to: "sessions#new", as: "login"
+    get '/login', to: "sessions#new", as: "login" #all good
     post '/login', to: "sessions#create"
-    post '/logout', to: "sessions#destroy"
+    post '/logout', to: "sessions#destroy" #all good
 
-    get "/auth/facebook/callback", to: "sessions#create_with_fb"
+    get "/auth/facebook/callback", to: "sessions#create_with_fb" #all good
 
-    get '/home', to: "readers#home"
+    get '/home', to: "readers#home" #working on it
 
     resources :readers, except: [:show, :index, :new, :create, :destroy, :patch, :put, :edit]  do
       resources :books, only: [:index] 
