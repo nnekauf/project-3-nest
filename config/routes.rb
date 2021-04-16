6a191194f2965b6be79078a74cd09e54f3584134
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-15 22:57:41 -0400

    adds log in and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,9 @@
     get '/', to: "readers#welcome"
     get '/signup', to: "readers#new", as: "signup"
     post '/signup', to: "readers#create"
-    # get '/login', to: "sessions#new", as: "login"
-    # post '/login', to: "sessions#create"
-  #   post '/logout', to: "sessions#destroy"
+    get '/login', to: "sessions#new", as: "login"
+    post '/login', to: "sessions#create"
+    post '/logout', to: "sessions#destroy"
     get "/auth/facebook/callback", to: "sessions#create_with_fb"
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
