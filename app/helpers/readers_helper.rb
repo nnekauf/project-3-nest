7a7d2654fef2880f777c8221c8795d12a6e464bc
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-16 08:49:38 -0400

    adds helpers

--- app/helpers/readers_helper.rb
+++ app/helpers/readers_helper.rb
@@ -1,2 +1,13 @@
 module ReadersHelper
+
+    def check_owner(obj)
+        obj.reader == current_user
+      end
+  
+      def redirect_if_not_owner(obj)
+        if !check_owner(obj)
+          flash[:message] = "Sorry, this is not your review!"
+          redirect "/home"
+        end
+      end
 end
